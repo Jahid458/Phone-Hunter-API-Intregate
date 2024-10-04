afc9473a106a205d4ce98ba44007e31fb65721fd
Jahid458 <jahidul898@outlook.com>
2024-10-05 01:40:04 +0600

    modalContainer Data Showing in show details

--- script.js
+++ script.js
@@ -70,9 +70,28 @@ const handleSearch = () => {
 
 
 const phoneDetails = async(slug) =>{
-    const response =await fetch(`https://openapi.programming-hero.com/api/phone/${slug}`);
+    const response = await fetch(`https://openapi.programming-hero.com/api/phone/${slug}`);
     const data = await response.json();
-    console.log(data.data)
+    console.log(data.data);
+
+    const modalContainer = document.getElementById('modal-container');
+    modalContainer.innerHTML = `
+    
+     <dialog id="my_modal_1" class="modal">
+    <div class="modal-box">
+      <h3 class="text-lg font-bold">Hello!</h3>
+      <p class="py-4">Press ESC key or click the button below to close</p>
+      <div class="modal-action">
+        <form method="dialog">
+          <!-- if there is a button in form, it will close the modal -->
+          <button class="btn">Close</button>
+        </form>
+      </div>
+    </div>
+  </dialog>
+    
+    `
+    my_modal_1.showModal()
 } 
 
 
